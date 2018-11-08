b20a7b0b91e34a63acf46a0b91047ec651cecb1c
MariusHendriks <Mark.hendriks9999@gmail.com>
2018-11-08 15:36:26 +0100

    Made a application that gets all PHP files in the directory 'Mensen'.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Leukste les ooit</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Github workshop</h1> 
+        <p>Als jullie goed opletten begrijpen jullie waarom en hoe je Github gebruikt!</p> 
+      </div>
+      <ul class="list-group">
+        <?php
+        if(glob("Mensen/*.php") != null){
+          foreach (glob("Mensen/*.php") as $gebruiker){
+            ?>
+            <li class="list-group-item">
+              <?php include $gebruiker; ?>
+            </li>
+          <?php } 
+            }else{
+              echo "<li class='list-group-item'>No users yet</li>";
+            }?>
+        
+      </ul>
+    </div>
+  </body>
+</html>
